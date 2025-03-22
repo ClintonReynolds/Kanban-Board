1977b04b45fdbad4555d502c44c2994c25d458fc
clinton reynolds <eastwood188209@gmail.com>
2025-03-22 17:56:48 -0400

    added code to authapi.tsx

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,26 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
-
-
+  try {
+    // TODO: make a POST request to the login route
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+    throw new Error('Login failed');
+  } catch (err) {
+    console.log("Error from user login: ", err);
+    return Promise.reject("Could not fetch user data.");
+  }
+};
 
 export { login };
+
+ 
