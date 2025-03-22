30c48ef886ec59365eb156d6912c910b90f51670
clinton reynolds <eastwood188209@gmail.com>
2025-03-22 17:46:53 -0400

    added code to authroutes

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,4 +7,23 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers['authorization'];
+  const token = authHeader?.slice(7) || '';
+
+  // if token decode it
+  // if no token send back error
+  if (!token){
+    res.sendStatus(401);
+    return;  
+  }
+  
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, user) => {
+    if (err) {
+      res.sendStatus(403);
+      return;
+    }
+    req.user = user as JwtPayload;
+    return next();
+  });
 };
+   
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,23 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+  const user = await User.findOne({
+    where: {
+      username: username 
+    }
+  });
+  if (!user) {
+    res.sendStatus(401);
+    return;
+  }
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) {
+    res.sendStatus(401);
+    return;
+  }
+  const token = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET as string);
+  res.json({ token });
 };
 
 const router = Router();
